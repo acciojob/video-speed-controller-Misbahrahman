4a7e3803e9ddf86e1d4cdcf7742c80cd463d9679
Misbahur rahman <98620184+Misbahrahman@users.noreply.github.com>
2023-12-22 21:32:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ const inputs = document.querySelectorAll('.controls input');
 
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+
+let ele = document.getElementById("vid");
+
+function forward(){
+    ele.currentTime += 10;
+}
+
+let e = document.getElementsByTagName("video");
+
+function backward(){
+    ele.currentTime -= 10;
+}
